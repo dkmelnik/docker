1d4c66469ea39ab90c1ada337e753e333812e780
Dmitriy Melnik <dmelnik@dengisrazy.ru>
2021-11-26 19:59:03 +0300

    #11. Делаем запросы к базе данных

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const {connectDB} = require("./helpers/db");
 const {port, host, db} = require("./configuration");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const postSchema = { name: String }
+const Post = mongoose.model("Post", postSchema);
 
 
 const startServer = () => {
   app.listen(port, () => {
-    console.clear()
+    const silence = new Post({ name: 'Zildjian' });
     console.log(`Started api service on port: ${port}`)
     console.log(`On host: ${host}`)
     console.log(`Our db: ${db}`)
+    Post.find(function (err,posts){
+      if (err) return console.log(err)
+      else console.log(posts)
+    })
+    silence.save(function (err, savedSilence){
+      if (err) return console.log(err)
+      else console.log(savedSilence)
+    })
   })
 }
 

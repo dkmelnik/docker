c45077ef7754f386d0ab26039303d076db51612c
Dmitriy Melnik <dmelnik@dengisrazy.ru>
2021-12-01 16:11:38 +0300

    #21. Последние улучшения

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !
+          Learn React!!!!22
         </a>
         <button onClick={() => {
           axios('/api/testwithcurruntuser').then(res => {
